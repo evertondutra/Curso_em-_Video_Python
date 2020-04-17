ab4d48ac9199090559a8470a0577140a998947f3
Everton Dutra <evertondutra02@gmail.com>
2020-04-17 18:34:36 -0300

    teste aula 18

---
+++ aula18.py
@@ -0,0 +1,28 @@
+"""
+teste = []
+teste.append('Gustavo')
+teste.append(40)
+print(teste)
+galera = []
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(teste)
+print(galera)
+"""
+"""
+galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos')
+"""
+galera = []
+dado = []
+for c in range(0, 3):
+    dado.append(input('NOME: '))
+    dado.append(int(input('IDADE: ')))
+    galera.append(dado[:])
+    dado.clear()
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade.')
+print(galera)
