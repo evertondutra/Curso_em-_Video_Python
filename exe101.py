c29a537aa37a94280388381467361bdcb8446bac
Everton Dutra <evertondutra02@gmail.com>
2020-05-12 15:08:34 -0300

    def com parâmetro, com return

---
+++ exe101.py
@@ -0,0 +1,20 @@
+"""
+Crie um programa que tenha uma função chamada voto() que vai
+receber como parâmetro o ano de nascimento de uma pessoa,
+retornando um valor literal indicando se uma pessoa tem voto
+NEGADO, OPCIONAL ou OBRIGATÓRIO NAS ELEIÇÕES.
+"""
+def voto(ano):
+    from datetime import date
+    idade = date.today().year - ano
+    if 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    elif idade >= 18:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+    else:
+        return f'Com {idade} anos: VOTO NEGADO.'
+
+
+print('=' * 30)
+nasc = int(input('Em que ano você nasceu? '))
+print(voto(nasc))
